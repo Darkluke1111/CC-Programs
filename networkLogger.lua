16b120761b93c20e7a746410db150d5e0b3487b6
Lukas <lukas.gromer@uni-ulm.de>
2021-09-27 14:39:43 +0200

    FIxes

--- networkLogger.lua
+++ networkLogger.lua
@@ -10,7 +10,7 @@ log = loggingLib
 function main()
   loggingPrio = 20
   if args[1] then
-    loggingPrio = args[1]
+    loggingPrio = tonumber(args[1])
   end
 
   log.setMinLoggingPrio(loggingPrio)
