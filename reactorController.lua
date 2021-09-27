a3e879b6c532daf92a495f3d9e0cd1d608729724
Lukas <lukas.gromer@uni-ulm.de>
2021-09-27 11:39:46 +0200

    Better Logging

--- reactorController.lua
+++ reactorController.lua
@@ -3,12 +3,17 @@
 function wrapReactor()
   for _,v in pairs(peripheral.getNames()) do
     if peripheral.getType(v) == "BigReactors-Reactor" then
+      logInfo("Wrapping Reactor at " ..  v)
       return peripheral.wrap(v)
     end
   end
-  reportError(msg)
+  logError(msg)
 end
 
-function reportError(msg)
-  write(msg .. "\n")
+function logError(msg)
+  write("[Error]" .. msg .. "\n")
+end
+
+function logInfo(msg)
+  write("[Info]" .. msg .. "\n")
 end
