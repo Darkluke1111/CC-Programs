01c5e88a44119c3d3ba38fd58ce8a24c99be2c00
Lukas <lukas.gromer@uni-ulm.de>
2021-09-28 16:11:02 +0200

    Fixes

--- networkLogger.lua
+++ networkLogger.lua
@@ -8,24 +8,19 @@ os.loadAPI("CC-Programs/loggingLib.lua")
 log = loggingLib
 
 function main()
-  loggingPrio = 20
+  local loggingPrio = 20
   if args[1] then
     loggingPrio = tonumber(args[1])
   end
 
-  log.setMinLoggingPrio(loggingPrio)
-  log.logNetwork()
-end
-
--- loggs messages received by the network (blocking)
-function logNetwork()
   while true do
     local senderID, msg = rednet.receive("Logging", 10)
-    if config.minLoggingPrio <= config.levels[loggingLevel].prio then
+    if loggingPrio <= config.levels[msg.type].prio then
       util.writeC("[" .. msg.type .. "] ", config.levels[msg.type].color)
       util.writeC("(" .. senderID .. ") " .. msg .. "\n", colors.white)
     end
   end
+
 end
 
 main()
