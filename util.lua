3086250a62e575a70f1d0623e4a6c9719c233792
Lukas <lukas.gromer@uni-ulm.de>
2021-09-28 09:59:00 +0200

    More table utils

--- util.lua
+++ util.lua
@@ -1,11 +1,35 @@
+
+-- Concurrency --
 function yield()
   os.queueEvent("fakeEvent");
   os.pullEvent();
 end
+
+
+-- Table Operations -- 
 
 function contains(arr, elem)
   for _,v in pairs(arr) do
     if v == elem then return true end
   end
   return false
+end
+
+function map(arr,f)
+  local newArr = {}
+  for k,v in pairs(arr) do
+    newArr[k] = f(v)
+  end
+  return newArr
+end
+
+-- arr has to be an array style table!
+function filter(arr,filter)
+  local newArr = {}
+  for _,v in ipairs(arr) do
+    if filter(v) then 
+      table.insert(newArr,v)
+    end
+  end
+  return newArr
 end
