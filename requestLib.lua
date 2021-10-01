c106909fee18f5bc642e03ff1dd064e1f8b79c39
Lukas <lukas.gromer@uni-ulm.de>
2021-10-01 16:43:39 +0200

    Added requestLib

---
+++ requestLib.lua
@@ -0,0 +1,18 @@
+args = {...}
+
+os.loadAPI("CC-Programs/util.lua")
+os.loadAPI("CC-Programs/loggingLib.lua")
+local log = loggingLib
+local util = util
+
+local protocol =  "InvAccess"
+
+
+function requestItem(name, amount)
+    local request = {}
+    request[1] = {
+        item = name,
+        amount =  amount
+    }
+    rednet.broadcast(request,protocol)
+end
