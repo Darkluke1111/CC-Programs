0c80e4c2fc0fcc4f42d95db70bfa812bb5a71eba
Lukas <lukas.gromer@uni-ulm.de>
2021-10-04 11:25:23 +0200

    Added push Item

--- inventoryManager.lua
+++ inventoryManager.lua
@@ -13,12 +13,19 @@ local playerInv = peripheral.find("inventoryManager")
 function handler(user, command, params)
     if command == "pull" then
         pullItem(params[2], tonumber(params[3]))
+    elseif command == "push" then
+        pushItem(params[2], tonumber(params[3]))
     end
 end
 
 function pullItem(item, count)
     me.exportItem({name = item, count = count}, "NORTH")
     playerInv.addItemToPlayer("SOUTH",count,-1,item)
+end
+
+function pushItem(item, count)
+    playerInv.removeItemFromPlayer("SOUTH", count, -1, item)
+    me.importItem({name = item, count = count}, "NORTH")
 end
 
 cc.listenForMessage(handler)
