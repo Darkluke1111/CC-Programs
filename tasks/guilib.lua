8d5ccdadc934d56111446ce05f85207cf968381f
Lukas <lukas.gromer@uni-ulm.de>
2021-10-11 13:24:39 +0200

    Button Object

--- tasks/guilib.lua
+++ tasks/guilib.lua
@@ -7,7 +7,23 @@ local height, width = m.getSize()
 local bgc = colors.black
 local fgc = colors.red
 
+local Button = {
+    width = 10,
+    height = 5,
+    text = "Button",
+    color = bgc
+}
+
 m.setTextScale(0.5)
+
+function Button.new(width, height, text, color)
+    local button = {}
+    setmetatable(button,Button)
+    button.width = width
+    button.height = height
+    button.text = text
+    button.color = color
+end
 
 function clear()
     m.setBackgroundColor(bgc)
@@ -27,34 +43,28 @@ function drawRect(xMin, yMin, xMax, yMax, color)
     end
 end
 
-function drawButton(xMin, yMin, xMax, yMax, text, color)
-    if not color then
-        color = fgc
-    end
-    if not text or text == "" then
-        log.debug("No text provided!")
-        drawRect(xMin, yMin, xMax, yMax, color)
-    end
-
-    local bw = xMax - xMin
-    if #text > bw then
-        local newLength = bw - 3 > 0 and bw - 3 or 0
+function drawButton(b, pos)
+    local text = b.text
+    local width = b.width
+    local height = b.height
+    if #text > width then
+        local newLength = width - 3 > 0 and width - 3 or 0
         text = string.sub(text, 1, newLength)
-        while #text < bw do
+        while #text < width do
             text = text .. "."
         end
     end
 
     local tw = #text
     log.debug("Textlength: " .. tw)
-    local yMid = math.floor((yMin + yMax) / 2) +1
-    local xMid = math.floor((xMin + xMax) / 2) +1
+    local xMid = math.floor(pos.x + width / 2) +1
+    local yMid = math.floor(pos.y + height/ 2) +1
     local xTextStart = math.floor(xMid - tw/2)
 
-    m.setBackgroundColor(color)
-    for i = yMin, yMax do
-        local j = xMin
-        while j <= xMax do
+    m.setBackgroundColor(b.color)
+    for i = pos.y, pos.y + height do
+        local j = pos.x
+        while j <= pos.x + width do
             if i == yMid and j == xTextStart then
                 m.setCursorPos(j, i)
                 m.write(text)
